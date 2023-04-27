27263df5c1dfc8561543cbbe1ac23e218fea6aa7
Petr Král <72036926+PrinceOfCzechia@users.noreply.github.com>
2023-04-27 17:38:33 +0200

    semi-functioning version added

--- nablaAuto.cpp
+++ nablaAuto.cpp
@@ -109,11 +109,12 @@ real L( const ArrayXreal& x )
     real L = 0.0;
     for( int i = 0; i < x.size(); i++ )
     {
-        L += ( x(4*i) - x(4*i+2) ) * ( x(4*i) - x(4*i+2) )
-           + ( x(4*i+1) - x(4*i+3) ) * ( x(4*i+1) - x(4*i+3) );
+        L += x(2*i) * x(2*i) + x(2*i+1) * x(2*i+1);
     }
     return L;
 }
+
+const int ITERATIONS_COUNT = 1e1;
 
 template< typename MeshConfig >
 bool nablaAuto( Mesh< MeshConfig, Devices::Host >& mesh, const std::string& fileName )
@@ -129,134 +130,120 @@ bool nablaAuto( Mesh< MeshConfig, Devices::Host >& mesh, const std::string& file
     const int facesCount = mesh.template getEntitiesCount< MeshType::getMeshDimension() - 1 >();
     const int cellsCount = mesh.template getEntitiesCount< MeshType::getMeshDimension() >();
 
-    Containers::Vector< PointType, Devices::Host > cellCenters ( cellsCount );
-    for(int i = 0; i < cellsCount; i++)
+    for( int iter = 0; iter < ITERATIONS_COUNT; iter++ )
     {
-        auto cell = mesh.template getEntity< MeshType::getMeshDimension() >( i );
-        PointType center = getEntityCenter( mesh, cell );
-    }
-
-    Containers::Vector< PointType, Devices::Host > faceCenters ( facesCount );
-    for(int i = 0; i < facesCount; i++)
-    {
-        auto face = mesh.template getEntity< MeshType::getMeshDimension() - 1 >( i );
-        PointType center = getEntityCenter( mesh, face );
-    }
-
-    Containers::Vector< PointType > nabla_h ( verticesCount );
-    Containers::Vector< PointType > nabla ( verticesCount );
-    nabla_h = 0;
-    nabla = 0;
-
-    auto get_nabla_h = [ &mesh, &nabla_h, &nabla ] ( GlobalIndexType i )  mutable
-    {
-        auto cell = mesh.template getEntity< MeshType::getMeshDimension() >( i );
-        PointType sum = { 0, 0 };
-        for( int j = 0; j < 3; j++ )
+        Containers::Vector< PointType, Devices::Host > cellCenters ( cellsCount );
+        for(int i = 0; i < cellsCount; i++)
         {
-            const auto faceIdx = cell.template getSubentityIndex< MeshType::getMeshDimension() - 1 >( j );
-            const auto sigma = mesh.template getEntity< MeshType::getMeshDimension() - 1 >( faceIdx );
-
-            PointType faceVector = mesh.getPoint( cell.template getSubentityIndex< 0 > ( (j+2) % 3 ) ) 
-                                 - mesh.getPoint( cell.template getSubentityIndex< 0 > ( (j+1) % 3 ) );
-
-            PointType outwardNormal = normalize< PointType >( { faceVector[ 1 ], -faceVector[ 0 ] } );
-
-            PointType x_sigma = getEntityCenter( mesh, sigma );
-
-            double f_sigma = f< PointType >( x_sigma );
-            sum += getEntityMeasure( mesh, sigma ) * f_sigma * outwardNormal;
+            auto cell = mesh.template getEntity< MeshType::getMeshDimension() >( i );
+            PointType center = getEntityCenter( mesh, cell );
         }
 
-        PointType grad_h = ( 1.0 / getEntityMeasure( mesh, cell ) ) * sum;
-        PointType grad = angrad< PointType >( getEntityCenter( mesh, cell ) );
-
-        for( int j = 0; j < 3; j++ )
+        Containers::Vector< PointType, Devices::Host > faceCenters ( facesCount );
+        for(int i = 0; i < facesCount; i++)
         {
-            int globalPointIdx = cell.template getSubentityIndex< 0 >( j );
-            nabla[ globalPointIdx ] += grad;
-            nabla_h[ globalPointIdx ] += grad_h;
+            auto face = mesh.template getEntity< MeshType::getMeshDimension() - 1 >( i );
+            PointType center = getEntityCenter( mesh, face );
         }
-    };
-    mesh.template forAll< MeshType::getMeshDimension() >( get_nabla_h );
 
-    using Eigen::VectorXd;
-    ArrayXreal var( 4 * verticesCount ); // the input array x with verticesCount variables
-    for( int i = 0; i < verticesCount; i++ ) // fill x
-    {
-        real aux = nabla_h[ i ][ 0 ];
-        var( 4*i ) = aux;
-        aux = nabla_h[ i ][ 1 ];
-        var( 4*i +1 ) = aux;
-        aux = nabla[ i ][ 0 ];
-        var( 4*i +2 ) = aux;
-        aux = nabla[ i ][ 1 ];
-        var( 4*i +3 ) = aux;
-    }
-    std::cout << "input array:\n" << var << "\n";
+        Containers::Vector< PointType > nabla_h ( verticesCount );
+        Containers::Vector< PointType > nabla ( verticesCount );
+        nabla_h = 0;
+        nabla = 0;
 
-    // computing L(mesh)
-    double loss = L( var );
-    
+        auto get_nabla_h = [ &mesh, &nabla_h, &nabla ] ( GlobalIndexType i )  mutable
+        {
+            auto cell = mesh.template getEntity< MeshType::getMeshDimension() >( i );
+            PointType sum = { 0, 0 };
+            for( int j = 0; j < 3; j++ )
+            {
+                const auto faceIdx = cell.template getSubentityIndex< MeshType::getMeshDimension() - 1 >( j );
+                const auto sigma = mesh.template getEntity< MeshType::getMeshDimension() - 1 >( faceIdx );
 
-    // give the points vector value of respective vertices
-    Containers::Vector< PointType > nabla_mesh( verticesCount );
-    nabla_mesh = 0;
+                PointType faceVector = mesh.getPoint( cell.template getSubentityIndex< 0 > ( (j+2) % 3 ) ) 
+                                    - mesh.getPoint( cell.template getSubentityIndex< 0 > ( (j+1) % 3 ) );
 
-    auto kernel = [ &mesh, &nabla_mesh, &nabla_h, &nabla, &var ] ( GlobalIndexType i )  mutable
-    {
+                PointType outwardNormal = normalize< PointType >( { faceVector[ 1 ], -faceVector[ 0 ] } );
+
+                PointType x_sigma = getEntityCenter( mesh, sigma );
+
+                double f_sigma = f< PointType >( x_sigma );
+                sum += getEntityMeasure( mesh, sigma ) * f_sigma * outwardNormal;
+            }
+
+            PointType grad_h = ( 1.0 / getEntityMeasure( mesh, cell ) ) * sum;
+            PointType grad = angrad< PointType >( getEntityCenter( mesh, cell ) );
+
+            for( int j = 0; j < 3; j++ )
+            {
+                int globalPointIdx = cell.template getSubentityIndex< 0 >( j );
+                nabla[ globalPointIdx ] += grad;
+                nabla_h[ globalPointIdx ] += grad_h;
+            }
+        };
+        mesh.template forAll< MeshType::getMeshDimension() >( get_nabla_h );
+
+        using Eigen::VectorXd;
+        ArrayXreal var( 2 * verticesCount ); // the input array x with verticesCount variables
+        for( int i = 0; i < verticesCount; i++ ) // fill x
+        {
+            real aux = nabla_h[ i ][ 0 ] - nabla[ i ][ 0 ];
+            var( 2*i ) = aux;
+            aux = nabla_h[ i ][ 1 ] - nabla[ i ][ 1 ];
+            var( 2*i +1 ) = aux;
+        }
+
+        // computing L(initial_mesh)
+        double loss = L( var );
         
-    };
-    mesh.template forAll< 0 >( kernel );
+
+        Containers::Vector< PointType > nabla_mesh( verticesCount );
+        nabla_mesh = 0;
+
+        real u;                                     // the output scalar u = L(x) evaluated together with gradient below
+
+        VectorXd g = gradient(L, wrt(var), at(var), u); // evaluate the function value u and its gradient vector g = du/dx
 
 
-    real u;                                     // the output scalar u = L(x) evaluated together with gradient below
+        for( int i = 0; i <= g.size(); i += 2 )
+        {
+            nabla_mesh[ i/2 ][ 0 ] = g( i );
+            nabla_mesh[ i/2 ][ 1 ] = g( i+1 );
+        }
 
-    VectorXd g = gradient(L, wrt(var), at(var), u); // evaluate the function value u and its gradient vector g = du/dx
 
-    std::cout << "u = " << u << "\n";      // print the evaluated output u
-    std::cout << verticesCount << " vertices" << "\n";
-    std::cout << "nabla_mesh of size " << g.size() << " = \n" << g << "\n";
+        Containers::Array< double > nabla_arr( 3 * verticesCount );
+        nabla_arr = 0;
+        for( int i = 0; i < 3 * verticesCount; i += 3 )
+        {
+            nabla_arr[ i ] = nabla_mesh[ i / 3 ][ 0 ];
+            nabla_arr[ i + 1 ] = nabla_mesh[ i / 3 ][ 1 ];
+        }
 
-    for( int i = 0; i <= g.size(); i += 2 )
-    {
-        nabla_mesh[ i/2 ][ 0 ] = g( i );
-        nabla_mesh[ i/2 ][ 1 ] = g( i+1 );
+        auto descent = [ &mesh, &nabla_mesh ] ( GlobalIndexType i ) mutable
+        {
+            mesh.getPoints()[ i ] -= 1e-3 * nabla_mesh[ i ]; // TODO change parameter
+        };
+        mesh.template forInterior< 0 >( descent );
+
+        // writing the computed gradient into a new mesh
+        using VTKWriter = Meshes::Writers::VTKWriter< MeshType >;
+        std::ofstream out = std::ofstream( "defGrads.vtk" );
+        VTKWriter writer = VTKWriter( out );
+        writer.template writeEntities< MeshType::getMeshDimension() >( mesh );
+        writer.writePointData( nabla_arr, "meshGrads", 3 );
+
+        nabla_h = 0;
+        nabla = 0;
+        mesh.template forAll< MeshType::getMeshDimension() >( get_nabla_h );
+        double new_loss = old_L< PointType >( nabla_h, nabla );
+
+        double improvement = loss - new_loss;
+        
+        std::cout << "L(initial mesh) = " << loss << "\n"
+                << "L(updated mesh) = " << new_loss << "\n"
+                << "\nimprovement of L: " << improvement << "\n\n";
     }
-
-
-    Containers::Array< double > nabla_arr( 3 * verticesCount );
-    nabla_arr = 0;
-    for( int i = 0; i < 3 * verticesCount; i += 3 )
-    {
-        nabla_arr[ i ] = nabla_mesh[ i / 3 ][ 0 ];
-        nabla_arr[ i + 1 ] = nabla_mesh[ i / 3 ][ 1 ];
-        nabla_arr[ i + 2 ] = 0; // redundant
-    }
-
-    auto descent = [ &mesh, &nabla_mesh ] ( GlobalIndexType i ) mutable
-    {
-        mesh.getPoints()[ i ] -= 1e-4 * nabla_mesh[ i ]; // TODO change parameter
-    };
-    mesh.template forInterior< 0 >( descent );
-
-    // writing the computed gradient into a new mesh
-    using VTKWriter = Meshes::Writers::VTKWriter< MeshType >;
-    std::ofstream out = std::ofstream( "defGrads.vtk" );
-    VTKWriter writer = VTKWriter( out );
-    writer.template writeEntities< MeshType::getMeshDimension() >( mesh );
-    writer.writePointData( nabla_arr, "meshGrads", 3 );
-
-    nabla_h = 0;
-    nabla = 0;
-    mesh.template forAll< MeshType::getMeshDimension() >( get_nabla_h );
-    double new_loss = old_L< PointType >( nabla_h, nabla );
-
-    double improvement = loss - new_loss;
-    
-    std::cout << "L(initial mesh) = " << loss << "\n"
-              << "L(updated mesh) = " << new_loss << "\n"
-              << "\nimprovement of L: " << improvement << "\n\n";
 
     std::cout << "OK" << "\n";
     return true;
